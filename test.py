166a3c57c2b39cbae928d24b313a93b4d4e1deb8
Alexander Chkliar <alex.chkliar@gmail.com>
2022-02-16 20:47:51 -0500

    reverse recursive

--- test.py
+++ test.py
@@ -1,22 +1,49 @@
-n = 10
-i_count = 0
-j_count = 0
-k_count = 0
-l_count = 0
-m_count = 0
+# n = 10
+# i_count = 0
+# j_count = 0
+# k_count = 0
+# l_count = 0
+# m_count = 0
 
-for i in range(0,n):
-    i_count += 1
-    for j in range(i,n):
-        j_count += 1
-        for k in range(j,n):
-            k_count += 1
-            for l in range(k,n):
-                l_count += 1
-                for m in range(l,n):
-                    m_count += 1
-print(i_count)
-print(j_count)
-print(k_count)
-print(l_count)
-print(m_count)
+# for i in range(0,n):
+#     i_count += 1
+#     for j in range(i,n):
+#         j_count += 1
+#         for k in range(j,n):
+#             k_count += 1
+#             for l in range(k,n):
+#                 l_count += 1
+#                 for m in range(l,n):
+#                     m_count += 1
+# print(i_count)
+# print(j_count)
+# print(k_count)
+# print(l_count)
+# print(m_count)
+
+# def reverseRecursive(str, index, output):
+#     if index == len(str):
+#         return output
+#     return reverseRecursive(str, index + 1, output + str[len(str) - index - 1])
+
+
+# print(reverseRecursive("ab", 0, ""))
+# print(reverseRecursive("abcde", 0, ""))
+
+# def reverseIterative(str):
+#     reversed_str = ""
+#     for i in range(0,len(str)):
+#         reversed_str += str[len(str) - 1 - i]
+#     return reversed_str
+
+# print(reverseIterative("ab"))
+# print(reverseIterative("abcde"))
+
+def fizzBuzz(integers):
+    for i in range(0, integers + 1):
+        if i % 5 == 0 and i % 3 == 0: print("FizzBuzz")
+        elif i % 3 == 0: print("Fizz")
+        elif i % 5 == 0: print("Buzz")
+        else: print(i)
+
+fizzBuzz(25)
